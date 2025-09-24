37995d147c8c3775b5cd71911994957f36187b03
dheenadhayalan <dheenadhayalan2004@gmail.com>
2025-09-24 15:57:32 +0530

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer'
+import Header from './Components/Header.jsx'
+import Footer from './Components/Footer'
 import Home from './pages/Home'
 import AboutUs from './pages/AboutUs'
 import ServicesPage from './pages/ServicesPage'
